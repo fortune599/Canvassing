5452f3fe23d1d91cd696daa98304fef9ddc832dd
Fortune Soleil <fsoleil@stuy.edu>
2017-02-14 18:15:45 -0500

    Add files via upload

--- canvasse.js
+++ canvasse.js
@@ -4,14 +4,25 @@ var clr = document.getElementById("clear");
 var ctx = c.getContext("2d");
 
 var clean = function(event) {
+  ctx.beginPath();
   ctx.fillStyle = "#ffffff";
   ctx.fillRect( 0, 0, 1300, 500 );
 };
 
-var shape = function(event) {
-  ctx.fillStyle = "#ff0000";
-  ctx.fillRect( event.clientX - 25, event.clientY - 25, 50, 50 );
+var path = function(event) {
+  ctx.lineTo(event.clientX, event.clientY);
+  shape(event);
+  ctx.stroke();
+  ctx.moveTo(event.clientX, event.clientY);
 };
 
+var shape = function(event) {
+  ctx.fillStyle = "#00ff00";
+  ctx.moveTo(event.clientX + 25, event.clientY);
+  ctx.arc( event.clientX, event.clientY, 25, 0, 2 * Math.PI );
+  ctx.fill()
+};
+
+ctx.beginPath();
 clr.addEventListener("mousedown", clean);
-c.addEventListener("mousedown", shape);
+c.addEventListener("mousedown", path);
